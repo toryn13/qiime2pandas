3ab494ec6a08ebef6027bd98e54ad73f028e4eab
Toryn Poolman <t.poolman@ucl.ac.uk>
2024-09-11 15:09:56 +0100

    Update __init__.py

--- qiime2pandas/__init__.py
+++ qiime2pandas/__init__.py
@@ -6,6 +6,9 @@ from .QZA_to_folder import unzip_qza_files
 from .rename_fastq import rename_fastq_files
 from .tax_sum import tax_glom
 from .tax_table import import_and_merge
+from .rare_curve import rarefaction_curve
+from .parse_sintax_and_merge import parse_sintax
+from .rarefy_otu_table import rarefy_otu_table
 
 # Optional: Expose functions in the package namespace
 __all__ = [
@@ -15,4 +18,7 @@ __all__ = [
     'rename_fastq_files',
     'tax_glom',
     'import_and_merge'
+    'rarefaction_curve',
+    'parse_sintax',
+'rarefy_otu_table'
 ]
